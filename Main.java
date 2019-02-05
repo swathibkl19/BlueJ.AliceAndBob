846b2cfa4ea97b57ce9661fdb0f4ac8c2f447937
Swathi Bikumalia <swathibikumalia@zipcoders-MacBook-Pro.local>
2019-02-05 13:48:23 -0500

    Greet msg to Alice and Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import java.util.*;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+       
+        if(name.equals("Alice"))
+        System.out.println("Hi Alice...");
+        else if(name.equals("Bob"))
+        System.out.println("Hi Bob...");
+        else
+        System.out.println("Hi");
+        
+        
+        
+   
     }
 }
